4cfd30576d8ee05af7010f4465c9e7bd230a1cd3
Yoseph kassaye <yadugna@gmail.com>
2022-01-02 17:29:21 -0800

    Iitial MVC Page

---
+++ SMS_MVC-Project-2022/SMS_MVC-Project-2022/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SMS_MVC_Project_2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
